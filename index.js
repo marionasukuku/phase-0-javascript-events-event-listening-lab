ad1761076c984cbb1a1f278356a047b628f041b1
Marion <marionasukuku22@gmail.com>
2022-11-29 22:52:10 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+    document.getElementById("button").addEventListener('click', ()=>{
+        alert("I am clicked")
+    })
+// function addingEventListener() {
+//     document.getElementById("button").addEventListener('click', ()=>{
+//         alert("I am clicked")
+//     })
+// }
+// addingEventListener();
+function clickAlert(){
+    return alert('I was clicked!');
+ }
+
+function addingEventListener(){
+    const input = document.getElementById('button');
+
+    input.addEventListener('click', clickAlert);
+}
+addingEventListener();
 }
